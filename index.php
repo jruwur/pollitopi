148f35ab842888700d1f7b0d2569488c41577de7
jruwur <118421142+jruwur@users.noreply.github.com>
2023-04-13 03:44:00 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/Iconopio.ico">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/cabeza.css">
+    <link rel="stylesheet" href="css/Inicio.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+    <script src="https://kit.fontawesome.com/094144b100.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https:/fonts.googleapis.com/css2?family=Josfin+Sans:ital,wght@0,.100;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Montserrat:wght@700;800;900&display=swap">
+    <title>inicio</title>
+</head>
+<body>
+<?php
+  include("database.php");
+  if($_GET['correoUsuario']){
+  }
+  $usuarioActivo= $_GET['correoUsuario'];
+  $sql ="select nombre from usuarios where correo = '$usuarioActivo'";
+  $stmnt = $conn ->query($sql);
+  $row = $stmnt -> fetch_assoc();
+  echo "<div class='alert alert-success text-center d-flex align-items-center' role='alert'>
+        <div>
+         Bienvenido ". $row['nombre'];"
+        </div>
+      </div>";
+  ?>
+<div class="hero">
+    <nav>
+        <img class="logotipo" src="img/logo-removebg-preview.png" alt="logos">
+        <ul>
+            <li><a href="#">inicio</a></li>
+            <li><a href="pages/menu.php">menu</a></li>
+            <li><a href="pages/productos.php">productos</a></li>
+            <li><a href="pages/ubication.php">ubicacion</a></li>
+            <script async src="https://cse.google.com/cse.js?cx=83e16dab7ae3d4ed5">
+            </script>
+            <div class="gcse-searchbox-only"></div>
+        </ul>
+        <a href="pages/login.php"><button type="button">Login</button></a>
+    </nav>
+</div>
+
+        <div class="imagenS">
+            <iframe style="margin-top: 20px;" width="560" height="315" src="https://www.youtube.com/embed/ptc4Awb0UpU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        
+        <section class="section">
+            <div class="container-1">
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx"><img src="img/Pollo.jpeg"></div>
+                        <div class="contentBx">
+                            <h3>POLLOS<br><span>Pollo asado</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li style="--i:1">
+                            <a href="" class="cta"><button>Ver Producto</button></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx"><img src="img/Flautas.jpeg"></div>
+                        <div class="contentBx">
+                            <h3>FLAUTAS<br><span>Flautas de pollo</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li style="--i:1">
+                            <a href="" class="cta"><button>Ver Producto</button></a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx"><img src="img/Alitas.jpeg"></div>
+                        <div class="contentBx">
+                            <h3>Alitas<br><span style="font-family: 'Montserrat', sans-serif;">Alitas adobadas con salsa especial de la casa</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li style="--i:1">
+                            <a href="" class="cta"><button>Ver productos</button></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        
+    </div>
+    <footer class="footer">
+<div class="container">
+    <div class="row">
+        <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+                <li><a href="#">Pollitos Pio</a></li>
+                
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+                <li><a href="pages/terms&conditions.html">Terminos y condiciones</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Redes sociales</h4>
+            <ul>
+                <li><a href="https://www.facebook.com/PollitotoPio/">Facebook</a></li>
+                <li><a href="https://www.instagram.com/ivan_buhaje/">Instragram</a></li>
+                <li><a href="https://twitter.com/pollitopiopoi3">Twitter</a></li>
+                <li><a href="https://www.youtube.com/@Natalan">Youtube</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>¡Siguenos!</h4>
+            <div class="social-links">
+                <a href="https://www.facebook.com/PollitotoPio/"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com/pollitopiopoi3"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/ivan_buhaje/"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/@Natalan"><i class="fab fa-youtube"></i></a>
+                
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+</footer>
+
+   
+</body>
+</html>
